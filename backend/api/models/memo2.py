e8332e59a118fe66b820152f79e51de06b5cc249
Takemoto-Tomohiro <s2322039@stu.musashino-u.ac.jp>
2024-07-27 16:16:24 +0900

    いろいろやった２

--- backend/api/models/memo2.py
+++ backend/api/models/memo2.py
@@ -3,12 +3,17 @@ import supabase_client
 async def create_memo(
         user_id: int,
         title: str,
-        related_products: list[dict, dict,],
+        related_products: list[dict],
         done: bool = False,
 ):
     async with supabase_client.SupabaseManager() as sbm:
         client = await sbm.get_client()
 
+        # 同じユーザーが未完了のメモを持っているか確認
+        existing_memo_resp = await client.table("memo").select("id").eq("user_id", user_id).eq("done", False).execute()
+        if existing_memo_resp.data:
+            return False
+        
         # インサートするデータを辞書形式で準備
         data = {
             "user_id": user_id,
@@ -25,12 +30,15 @@ async def create_memo(
                 await client.table("product").insert(product).execute()
 
         await insert_products(related_products)
+
+        return True
     
 
 items = [{"name":"冷やし豆腐"}, {"name":"るーろーはん"}]
 
 async def test():
-    await create_memo(1, "ちわっす", items)
+    a = await create_memo(1, "ちわっす", items)
+    print(a)
 
 import asyncio
 asyncio.run(test())
--- backend/api/models/user.py
+++ backend/api/models/user.py
@@ -1,9 +1,11 @@
 from . import supabase_client
 import structlog
+from supabase import PostgrestAPIError
+from fastapi import HTTPException
 
 logger = structlog.get_logger()
 
-async def registUser(mail_addres: str, password: str) -> dict:
+async def registUser(mail_addres: str, password: str) -> int:
     """
     新しいユーザーを登録する。
     """
@@ -11,15 +13,19 @@ async def registUser(mail_addres: str, password: str) -> dict:
         async with supabase_client.SupabaseManager() as manager:
             db_client = await manager.get_client()
             response = await db_client.table("user").insert({"mail_addres": mail_addres, "password": password}).execute()
-            return response.data
-    except:
-        return False
+            return response.data[0]["id"]
+    except PostgrestAPIError:
+        raise HTTPException(status_code=400, detail={"message": "多分同じメアドがすでに灯籠されているっぽい…"})
+    
+    except Exception as e:
+        raise HTTPException(status_code=400, detail={"message": f"予期せぬエラーってやつ…？あはは…:{e}"})
+    
     
 class UserClient:
     def __init__(self) -> None:
         pass
 
-    async def find(self, mail_addres: str, password: str) -> list[dict, dict,]:
+    async def find(self, mail_addres: str, password: str):
         """
         空のリストの場合は該当がなかったということ。
         """
@@ -28,20 +34,16 @@ class UserClient:
                 db_client = await manager.get_client()
                 response = await db_client.table("user").select("id").eq("mail_addres", mail_addres).eq("password", password).execute()
                 if len(response.data) > 0:#>0としているけど、それが2以上になることはないはず
-                    return response.data
-                else:#長さ0の場合、条件に合うuserがいないということ。
-                    return False
-        except Exception as e:
-            logger.error(e)
-            raise
+                    return response.data[0]["id"]
+                    
+        except Exception:
+            raise HTTPException(status_code=400, detail={"message" : "メアドかパスワードが間違えているか、そもそもまだ登録していないか…"})
 
     async def updateUserData(self):
         pass
 
 async def main():
-    # resp = await registUser("example12345@mail.com", "password123")
-    # if not resp:
-    #     logger.info("同じメアドがすでに登録されています！")
+    #resp = await registUser("example12345@mail.com", "password123")
         
     user_client = UserClient()
     user = await user_client.find("example12345@mail.com", "password123")
