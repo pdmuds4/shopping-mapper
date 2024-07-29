2b3063a0948218fd17b3c203efe3edf19ce97b41
Takemoto-Tomohiro <s2322039@stu.musashino-u.ac.jp>
2024-07-29 10:18:09 +0900

    corsの設定と、keyの設定

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette.status import HTTP_403_FORBIDDEN
+from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
 from .routes import memo, user, product
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+API_KEY = "nOe2YOhyCfzhqv78wnEEBKQy9powjshU"
+API_KEY_NAME = "access_token"
+DOCS_USERNAME = "muds2022"
+DOCS_PASSWORD = "mIkzek-9dyrha-vobfyn"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+security = HTTPBasic()
+
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == API_KEY:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+
+async def verify_docs_credentials(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = credentials.username == DOCS_USERNAME
+    correct_password = credentials.password == DOCS_PASSWORD
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Invalid credentials"
+        )
+    return credentials.username
 
 app.include_router(memo.router, prefix="/memo", tags=["memo"])
 app.include_router(user.router, prefix="/user", tags=["user"])
@@ -10,3 +51,15 @@ app.include_router(product.router, prefix="/product", tags=["product"])
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html(credentials: HTTPBasicCredentials = Depends(verify_docs_credentials)):
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html(credentials: HTTPBasicCredentials = Depends(verify_docs_credentials)):
+    return get_redoc_html(openapi_url="/openapi.json", title="ReDoc")
+
+@app.get("/openapi.json", include_in_schema=False)
+async def get_open_api_endpoint(credentials: HTTPBasicCredentials = Depends(verify_docs_credentials)):
+    return JSONResponse(get_openapi(title="FastAPI", version="1.0.0", routes=app.routes))
